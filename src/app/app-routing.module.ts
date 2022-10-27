a7cb06da936d2e50c3fc70a289969b6e83312794
NastiaHerych <nastia.herych@gmail.com>
2022-10-27 17:05:51 +0300

    angular guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,15 +12,19 @@ import { OnDeleteComponent } from './components/on-delete/on-delete.component';
 import { UpdatePageComponent } from './components/update-page/update-page.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard'
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['dashboard']);
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginPageComponent },
-  { path: 'sign-up', component: SignUpComponent },
+  { path: 'login', component: LoginPageComponent,  ...canActivate(redirectLoggedInToHome), },
+  { path: 'sign-up', component: SignUpComponent,  ...canActivate(redirectLoggedInToHome), },
   { path: 'footer', component: FooterComponent },
   { path: 'blueSection', component: BlueSectionComponent },
   { path: 'create', component: FormComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent,  ...canActivate(redirectUnauthorizedToLogin), },
   { path: 'created', component: OnCreatedComponent },
   { path: 'deleted', component: OnDeleteComponent },
   { path: 'update/:id', component: UpdatePageComponent },
